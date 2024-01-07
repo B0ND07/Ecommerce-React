1a49e705a18abc722fb1c6113f6c1da727084f19
b0nd07 <mhashimkp@gmail.com>
2024-01-07 22:20:35 +0530

    ecom: initial

---
+++ src/Ecommerce.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Shop from "./Components/Pages/Shop";
+import ShopCategory from "./Components/Pages/ShopCategory";
+import Product from "./Components/Pages/Product";
+import Cart from "./Components/Pages/Cart";
+import LoginSignup from "./Components/Pages/LoginSignup";
+
+const Ecommerce = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<><Shop/></>}/>
+        <Route path="/men" element={<><ShopCategory category="men"/></>}/>
+        <Route path="/women" element={<><ShopCategory category="women"/></>}/>
+        <Route path="/kids" element={<><ShopCategory category="kids"/></>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path=":productId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<LoginSignup/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Ecommerce;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Ecommerce from "./Ecommerce.jsx";
+import "../index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Ecommerce />
+  </React.StrictMode>
+);
